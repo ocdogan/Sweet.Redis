69620050db140e22ee1696f640fd44e5420e435f
cagataydogan <ocdogan@gmail.com>
2017-09-10 16:06:27 +0300

    Regis command helps added as comment

--- Sweet.Redis/Commands/IRedisConnectionCommands.cs
+++ Sweet.Redis/Commands/IRedisConnectionCommands.cs
@@ -1,5 +1,26 @@
 ﻿namespace Sweet.Redis
 {
+    /*
+    AUTH password
+    summary: Authenticate to the server
+    since: 1.0.0
+
+    ECHO message
+    summary: Echo the given string
+    since: 1.0.0
+
+    PING [message]
+    summary: Ping the server
+    since: 1.0.0
+
+    QUIT -
+    summary: Close the connection
+    since: 1.0.0
+
+    SELECT index
+    summary: Change the selected database for the current connection
+    since: 1.0.0
+     */
     public interface IRedisConnectionCommands
     {
         bool Auth(string password);
--- Sweet.Redis/Commands/IRedisScriptingCommands.cs
+++ Sweet.Redis/Commands/IRedisScriptingCommands.cs
@@ -1,5 +1,34 @@
 ﻿namespace Sweet.Redis
 {
+    /*
+    EVAL script numkeys key [key ...] arg [arg ...]
+    summary: Execute a Lua script server side
+    since: 2.6.0
+
+    EVALSHA sha1 numkeys key [key ...] arg [arg ...]
+    summary: Execute a Lua script server side
+    since: 2.6.0
+
+    SCRIPT DEBUG YES|SYNC|NO
+    summary: Set the debug mode for executed scripts.
+    since: 3.2.0
+
+    SCRIPT EXISTS script [script ...]
+    summary: Check existence of scripts in the script cache.
+    since: 2.6.0
+
+    SCRIPT FLUSH -
+    summary: Remove all the scripts from the script cache.
+    since: 2.6.0
+
+    SCRIPT KILL -
+    summary: Kill the script currently in execution.
+    since: 2.6.0
+
+    SCRIPT LOAD script
+    summary: Load the specified Lua script into the script cache.
+    since: 2.6.0
+     */
     public interface IRedisScriptingCommands
     {
         RedisObject Eval(string script, params RedisKeyValue<string, byte[]>[] args);
